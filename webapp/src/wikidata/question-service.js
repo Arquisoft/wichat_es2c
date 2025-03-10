e7122a2da0d9f5cf2a93a8e9d6641f42e9d97fe7
HernandezIglesiasHector <hhernandeziglesias@gmail.com>
2025-03-10 14:03:56 +0100

    WikiData-BackEnd

---
+++ webapp/src/wikidata/question-service.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const {generateQuestion}= require('/question-Generator.js');
+const app = express();
+
+const PORT = 3005;
+app.use(cors());
+app.use (express.json());
+app.get('/question', async (req, res) => {
+    try{
+        const question = await generateQuestion();
+        res.json(question);
+    }
+
+    catch (error)
+    {
+        res.status(500)-json({ error: 'Failed to generate question' });
+    }
+
+});
+app.listen(PORT, () => {
+    console. log(`Question Service running on port ${PORT} `);
+});
