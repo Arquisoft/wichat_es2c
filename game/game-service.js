2f93e2ad914a95dce55f9f8def29f89719a049d8
LucasGisb <UO295526@uniovi.es>
2025-04-28 01:07:02 +0200

    Difficulty now is shown at the game summarys. Also, improved thir look quite drastically

--- game/game-service.js
+++ game/game-service.js
@@ -208,7 +208,9 @@ app.get('/userMatches', async (req, res) => {
         score: match.score,
         correctAnswers,
         wrongAnswers,
-        questions: match.questions || []
+        questions: match.questions || [],
+
+        difficulty: match.difficulty
       };
     });
 
--- webapp/src/components/GameSummary.jsx
+++ webapp/src/components/GameSummary.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styles from "./GameSummary.module.css";
 import QuestionModal from "./Questions"; // Importa el modal
 
-export const GameSummary = ({ date, hour, correctAnswers, wrongAnswers, time, questions }) => {
+export const GameSummary = ({ date, hour, correctAnswers, wrongAnswers, time, questions, difficulty }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const openModal = () => setIsModalOpen(true);
@@ -25,10 +25,23 @@ export const GameSummary = ({ date, hour, correctAnswers, wrongAnswers, time, qu
                 <h3 className={styles.wrongAnswersElement}>{wrongAnswers}</h3>
             </div>
 
+
+            <div className={styles.difficultyElement}>
+                <h3 className={styles.difficultyLabel}>
+                    <span className={`${styles.difficultyValue} ${
+                    difficulty === 2 ? styles.hardDifficulty : styles.normalDifficulty
+                    }`}>
+                    {difficulty === 2 ? 'Hard' : 'Normal'}
+                    </span>
+                </h3>
+            </div>
+
+
             <div className={styles.TimesElement}>
                 <h3 className={styles.totalTime}>Total time: {time}</h3>
                 <h3 className={styles.timePerQuestion}>Time per question: {timePerQuestion}</h3>
             </div>
+
 
             <div className={styles.QuestionButtonElement}>
                 <button className={styles.questionButton} onClick={openModal}>
@@ -38,6 +51,8 @@ export const GameSummary = ({ date, hour, correctAnswers, wrongAnswers, time, qu
 
             {/* Aquí pasas las preguntas de manera correcta */}
             <QuestionModal isOpen={isModalOpen} closeModal={closeModal} questions={questions} />
+
+            
         </div>
     );
 
